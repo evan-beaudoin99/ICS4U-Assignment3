c4f00dcf90449034a8f694cdc4a26db70b85eaf1
Evan Beaudoin <124371659+evan-beaudoin99@users.noreply.github.com>
2024-04-27 12:12:41 -0400

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -108,4 +108,39 @@ final class Main {
         System.out.println(" → Circumcircle radius: " + String.format("%.4f", testTriangle3.circumsicleRadius()) + " mm");
         System.out.println();
 
+         // Test Triangle 4
+        Triangle testTriangle4 = new Triangle(2, 3, 4);
+        System.out.println("Created a 2 mm by 3 mm by 4 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle3.isValid());
+        System.out.println(" → Semiperimeter: " + String.format("%.4f", testTriangle3.semiPerimeter()) + " mm");
+        System.out.println(" → Area: " + String.format("%.4f", testTriangle3.area()) + " mm²");
+        System.out.println(" → Type: " + testTriangle3.getType());
+        System.out.println(" → Angle 1: " + String.format("%.4f", testTriangle3.angle(1)) + " rad");
+        System.out.println(" → Angle 2: " + String.format("%.4f", testTriangle3.angle(2)) + " rad");
+        System.out.println(" → Angle 3: " + String.format("%.4f", testTriangle3.angle(3)) + " rad");
+        System.out.println(" → Height 1: " + String.format("%.4f", testTriangle3.height(1)) + " mm");
+        System.out.println(" → Height 2: " + String.format("%.4f", testTriangle3.height(2)) + " mm");
+        System.out.println(" → Height 3: " + String.format("%.4f", testTriangle3.height(3)) + " mm");
+        System.out.println(" → Inner circle radius: " + String.format("%.4f", testTriangle3.innerCircleRadius()) + " mm");
+        System.out.println(" → Circumcircle radius: " + String.format("%.4f", testTriangle3.circumsicleRadius()) + " mm");
+        System.out.println();
+
+         // Test Triangle 5
+        Triangle testTriangle5 = new Triangle(23, 3, 4);
+        System.out.println("Created a 23 mm by 3 mm by 4 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle3.isValid());
+        System.out.println(" → Semiperimeter: " + String.format("%.4f", testTriangle3.semiPerimeter()) + " mm");
+        System.out.println(" → Area: " + String.format("%.4f", testTriangle3.area()) + " mm²");
+        System.out.println(" → Type: " + testTriangle3.getType());
+        System.out.println(" → Angle 1: " + String.format("%.4f", testTriangle3.angle(1)) + " rad");
+        System.out.println(" → Angle 2: " + String.format("%.4f", testTriangle3.angle(2)) + " rad");
+        System.out.println(" → Angle 3: " + String.format("%.4f", testTriangle3.angle(3)) + " rad");
+        System.out.println(" → Height 1: " + String.format("%.4f", testTriangle3.height(1)) + " mm");
+        System.out.println(" → Height 2: " + String.format("%.4f", testTriangle3.height(2)) + " mm");
+        System.out.println(" → Height 3: " + String.format("%.4f", testTriangle3.height(3)) + " mm");
+        System.out.println(" → Inner circle radius: " + String.format("%.4f", testTriangle3.innerCircleRadius()) + " mm");
+        System.out.println(" → Circumcircle radius: " + String.format("%.4f", testTriangle3.circumsicleRadius()) + " mm");
+        System.out.println();
+
+        System.out.println(Done);
 }
